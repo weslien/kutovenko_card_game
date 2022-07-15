b3affcb9d830b06bd8cd1487b9d71f93bb8bcf74
Gustav Weslien <gustav.weslien@gmail.com>
2022-07-15 11:29:09 +0200

    Feedback part 2
    Unfortunately, we collectively decided to not proceed further. Thanks for your time.

--- card_game.dart
+++ card_game.dart
@@ -25,6 +25,23 @@ class Game {
   /// So, here the business logic of the deck and cards is inline in the Game class. The choice of having only 1-10 of spades instead of A-K of 
   /// hearts, diamonds, clubs and spades just feels lazy. While there is a reasoning behind it, I really don't understand it. The least controversial
   /// choice would have been a standard deck of 52 cards. 
+  /// Additionally, typing sequences of numbers isn't great.
+  /*
+  Using constructs like these woiuld have been preferrable:
+  
+  List<int> cardIndex = List.generate(14, (index) => index + 1); 
+  =>
+  [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+  
+  Map valuesLongMap = {'1': 'Ace', '11': 'Jack', '12': 'Queen', '13': 'King'};
+  List<String> suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
+  Map suitsMap = {'Hearts': '♥', 'Diamonds': '♦', 'Spades': '♠', 'Clubs': '♣'};
+  
+  From these you could then easily have generated any number and combinations of decks, cards, etc.
+  
+  Many card games utilize several decks.
+  
+  */
   final List<String> deckOfCards = [
     '1 of spades',
     '2 of spades',
@@ -40,7 +57,7 @@ class Game {
 
   void _startGame(int numberOfPlayers, int numberOfCards) {
     players = List.generate(
-        numberOfPlayers, (index) => Player(name: 'Player $index', cards: []));
+        numberOfPlayers, (index) => Player(name: 'Player $index', cards: [])); /// This is good, perhaps 
 
     _validate(numberOfPlayers, numberOfCards)
         ? _giveCards(numberOfCards)
@@ -52,6 +69,24 @@ class Game {
     // not letters, doubles, etc. If we are still on Earth,
     // here should be input validation task for upcoming sprint.
     // First for QA engineer, if we have one, and then for developer.
+
+    /// We don't have any QA Engineers, nor will we. Developers should write working code
+    /// and automated tests (also written by developers) should verify this. Always sanitize and validate input.
+    
+    
+    
+    /* This code would have been more readable something like this:
+      
+      int numOfPlayers = _readInt('Please, print a number of players:');
+      int numOfPlayers = _readInt('Please, print number of cards per player:')
+      
+      int _readInt(String message){
+        print(message);
+        return int.parse(stdin.readLineSync()!);
+        
+      }
+      
+      */
     print('Print \'$PASSPHRASE\' to start a game or \'q\' to exit');
     if (stdin.readLineSync() == PASSPHRASE) {
       print('Please, print a number of players:');
@@ -59,16 +94,20 @@ class Game {
 
       print('Please, print number of cards per player:');
       int numOfCards = int.parse(stdin.readLineSync()!);
-
+    
+      
+      
+      
       _startGame(numOfPlayers, numOfCards);
     } else if (stdin.readLineSync() == 'q') {
       return;
     } else {
       print('We are doomed!'); //...or some kind of fancy custom Exception
+      /// While it is obvious you intentionally simplified, the amount of extra effort required to make it understandable is negligible. 
     }
   }
 
-  void _giveCards(int numberOfCards) {
+  void _giveCards(int numberOfCards) { /// commonly known as _dealCards
     print('Shuffling cards...');
     deckOfCards.shuffle();
     print('Ready!');
@@ -76,17 +115,34 @@ class Game {
 
     final playerCards = [];
 
+    /// This is not really how you deal cards. You go player by player, dealing each a card from the top of the deck, 
+    /// until all players have the correct number of cards.
+    
     for (int i = 0; i < len; i += numberOfCards) {
       var end = (i + numberOfCards < len) ? i + numberOfCards : len;
       playerCards.add(deckOfCards.sublist(i, end));
     }
 
+    /// This is pretty unreadable and most of us needed to put in extra effort to make sense of it.
     for (int i = 0; i < players.length; i++) {
       players[i].cards.addAll(playerCards[i]);
     }
 
-    _printStash(players);
+    _printStash(players); /// Strange naming choice
     intro();
+    /* Call stack would be
+      main()
+        Game.intro()
+          Game._startGame()
+            Game._giveCards()
+              Game.intro()
+                Game._startGame()
+                  Game._giveCards()
+                    Game.intro()
+                      etc... infinitely recursive stack
+    
+    */
+    
   }
 
   void _printStash(List<Player> players) {
@@ -103,3 +159,6 @@ class Game {
     return false;
   }
 }
+
+
+/// In general, sadly, not great. Feels a bit nonchalant.
